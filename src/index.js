7450c76abb15104b4515293a03d0292ae82039c8
am1t0 <amiitt43@gmail.com>
2024-01-27 16:58:03 +0530

    connect to mongoDB Atlas

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import 'dotenv/config'
+import connectDB from "./db/db.js";
+
+
+connectDB();
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express"
+const app = express();
+
+
+;(async () =>{
+    try{
+    await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+    app.on("error",(error)=>{
+        console.log("ERROR: ", error);
+        throw error
+    })
+    app.listen(process.env.PORT, ()=>{
+        console.log(`App is listening on port ${process.env.PORT}`)
+    })
+    }
+    catch(error){
+        console.log("ERROR: ",error)
+    }
+})()
+*/
